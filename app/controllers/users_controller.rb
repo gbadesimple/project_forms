52834616a36e56daecd8e198fd43350bf48df9fb
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-06 09:30:52 +0100

    Railsy-er forms with #form_for task complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   include UsersHelper
 
   def new
-
+    @user = User.new
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-  #  @user = User.new(user_params)
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
