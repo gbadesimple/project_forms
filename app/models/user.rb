04d7b88483cac16c135ab10f1b70da406fae662b
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-07 09:10:59 +0100

    Extra Credit task complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: {maximum: 15}
   validates :email, presence: true, uniqueness: true, length: { maximum: 200 }
-  validates :password, presence: true, length: { maximum: 10 }
-  
+  validates :password, presence: true, length: { minimum: 10 }
+
 end
