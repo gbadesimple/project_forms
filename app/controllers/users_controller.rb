a2dcaf0057eaec9cb5969abbd59d31c72682bc47
gbadesimple <olugbadeolalekan@yahoo.com>
2020-05-07 08:47:15 +0100

    Editing task complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   include UsersHelper
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -14,7 +18,21 @@ class UsersController < ApplicationController
     else
       render :new
     end
-
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
 
 end
